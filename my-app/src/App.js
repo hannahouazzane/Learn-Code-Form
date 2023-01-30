c0fb08e494f6e0e869441368f2a4ab7008b3f017
hannahouazzane <hannahouazzane@gmail.com>
2023-01-30 14:08:32 +0000

    add: grid with responsive boxes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,82 @@
-
-
+import React from 'react';
+import "./App.css"
+import Square from "./Components/Square"
+import  { useState } from 'react';
 function App() {
+
+  const [user, setUser] = useState("")
+
+  function changeSquare () {
+    setUser("X")
+
+
+  }
+
+ 
+
+
+    
+   
+
+   
+
+
   return (
-    <div>hi</div>
+   <div className="App">
+
+    <div className="board">
+
+      <div className="row">
+        <Square 
+          changeSquare= {()=> {
+            alert(0)
+          }}
+          user="{user}"
+        
+        
+        />
+        <Square 
+          changeSquare= {()=> {
+            alert(1)
+          }}
+          user="{user}"
+        
+        
+        />
+        <Square 
+          changeSquare= {()=> {
+            alert(2)
+          }}
+          user="{user}"
+        
+        
+        />
+        
+        
+          
+      </div>
+
+      <div className="row">
+        
+        
+          
+      </div>
+
+      <div className="row">
+        
+        
+          
+      </div>
+  
+  
+
+
+    </div>
+
+
+  </div>
   );
 }
+
 
 export default App;
---
+++ my-app/src/Components/Square.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import "../App.css";
+
+const Square = ( {name,user, changeSquare}) => {
+  
+   
+    return( 
+        <div className="square" onClick={changeSquare} >
+            
+            <p>{user}</p>
+            <p>{name}</p>
+      
+
+
+        </div>
+
+
+
+    )
+
+
+
+
+};
+
+Square.propTypes = {
+    name: PropTypes.string
+
+};
+
+export default Square;
