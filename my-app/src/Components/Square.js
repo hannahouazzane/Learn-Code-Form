2a9fe8f1e5ad96d2b3c1824d4bee447478eb2af9
hannahouazzane <hannahouazzane@gmail.com>
2023-02-06 15:48:49 +0000

    change: folder organisation - created page folder

--- my-app/src/Components/Square.js
+++ my-app/src/Components/Square.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "../App.css";
+import "../Pages/Game/App.css";
 
 const Square = ({ name, user, changeSquare }) => {
   return (
